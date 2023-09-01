344d94d73b51781053d4500ef4b92399a5a034c9
Jun <neohwj28@gmail.com>
2023-09-02 01:22:16 +0800

    Add search and sort algorithms

---
+++ Chapter 3 - Introduction To Algorithms/Assignment 4 - Searching Algorithms/lab4/binary-search.js
@@ -0,0 +1,62 @@
+const assert = require("assert");
+
+// Task: Complete the function binarySearch() that search for an target in the input array
+// Return the found position if found
+// Return the -1 if not found
+
+const binarySearch = (arr, target) => {
+	// Add your code here
+	let start = 0;
+	let end = arr.length;
+
+	do {
+		const middle = Math.floor(start + (end - start) / 2);
+		const value = arr[middle];
+
+		if (value === target) {
+			return middle;
+		} else if (value < target) {
+			start = middle + 1;
+		} else if (value > target) {
+			end = middle;
+		}
+	} while (start < end);
+
+	return -1;
+};
+
+// DO NOT MODIFY CODE BELOW
+// Test Cases
+const inputArray = [1, 2, 4, 6, 7, 9, 10, 11, 13];
+
+assert.strictEqual(
+	binarySearch(inputArray, 3),
+	-1,
+	"Test case 1 output is wrong."
+);
+assert.strictEqual(
+	binarySearch(inputArray, 1),
+	0,
+	"Test case 2 output is wrong."
+);
+assert.strictEqual(
+	binarySearch(inputArray, 4),
+	2,
+	"Test case 3 output is wrong."
+);
+assert.strictEqual(
+	binarySearch(inputArray, 7),
+	4,
+	"Test case 4 output is wrong."
+);
+assert.strictEqual(
+	binarySearch(inputArray, 10),
+	6,
+	"Test case 5 output is wrong."
+);
+assert.strictEqual(
+	binarySearch(inputArray, 13),
+	8,
+	"Test case 6 output is wrong."
+);
+console.log("All test cases passed.");
